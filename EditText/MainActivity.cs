45a2da13c7333cac31ae5982ea7aadd56d7ca28d
Twan Julicher <twan_1993@hotmail.com>
2014-04-11 09:52:09 +0200

    start project google keep

---
+++ EditText/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace EditTextDemo
+{
+	[Activity (Label = "EditText", MainLauncher = true)]
+	public class Activity1 : Activity
+	{
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			var editText = FindViewById<EditText> (Resource.Id.editText);
+			var textView = FindViewById<TextView> (Resource.Id.textView);
+
+			editText.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {      
+				textView.Text = e.Text.ToString ();
+			};
+		}
+	}
+}
+
+
+
